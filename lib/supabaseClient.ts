e2fbfd9d11e393e20834e96d30aaaf0c19301446
Peter Nortey <ecexpay@gmail.com>
2023-02-20 10:46:29 +0000

    ref(document): renamed document under pages

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 
+const SUPABASE_URL = 'https://dyhggeyvegxqqfxumyhc.supabase.co'
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5aGdnZXl2ZWd4cXFmeHVteWhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzUyMzk3OTQsImV4cCI6MTk5MDgxNTc5NH0.5dXir1I3aUymbiAS-U2b4oY_FCFWst7Yfi-A5FoSXaE"
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY,
 )
