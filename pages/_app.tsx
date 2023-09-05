41a66c8deb2e874c73360fe9c627a9f069675ddf
pnoken <ecexpay@gmail.com>
2023-09-05 18:16:18 +0000

    fix(app/supabase): removed supabase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import { SessionContextProvider, Session } from '@supabase/auth-helpers-react';
 import { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import '../styles/main.css';
@@ -8,21 +5,12 @@ import '../styles/main.css';
 function MyApp({
 	Component,
 	pageProps,
-}: AppProps<{
-	initialSession: Session;
-}>) {
-	const [supabase] = useState(() => createBrowserSupabaseClient());
-
+}: AppProps) {
 	return (
-		<SessionContextProvider
-			supabaseClient={supabase}
-			initialSession={pageProps.initialSession}
-		>
-			{/* <Annoucement /> */}
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</SessionContextProvider>
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
+
 	);
 }
 export default MyApp;
