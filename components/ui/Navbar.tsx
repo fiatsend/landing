221ebcb7e4f32ca9ac7f7ea37909928ec14e14ad
pnoken <norteypeter@gmail.com>
2024-03-05 12:11:35 +0000

    feat(bridge): integrated wormhole bridge

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -4,7 +4,7 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
 const navigation = [
 	{ name: 'Bridge ↗', href: 'bridge', newWindow: true },
-	{ name: 'Pools (Coming Soon)', href: '#pools' },
+	{ name: 'Pools (Coming Soon)', href: '#pools', newWindow: false },
 ];
 
 const Navbar = () => {
