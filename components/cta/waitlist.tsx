b0bd03b03dca1776105629303262fe8f7d834865
pnoken <norteypeter@gmail.com>
2024-07-17 10:33:20 +0000

    chore(banner): add content to banner

--- components/cta/waitlist.tsx
+++ components/cta/waitlist.tsx
@@ -13,12 +13,19 @@ export const Waitlist = () => {
 						<div className="mx-auto max-w-md px-4 text-center sm:max-w-2xl sm:px-6 lg:flex lg:items-center lg:px-0 lg:text-left">
 							<div className="lg:py-24">
 								<h1 className="mt-4 text-4xl font-bold tracking-tight text-black sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
-									<span className="block text-blue-500">Sending money </span>
-									<span className="block text-black text-xl">hasn't been that easy</span>
+									<span className="block text-blue-500">Send money </span>
+									<span className="block text-black text-xl">in one click</span>
 								</h1>
 								<p className="mt-3 text-base text-gray-400 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-									We simplify the process in just one click
+									Works faster than the speed of light
 								</p>
+								<button
+									type="button"
+									disabled
+									className="bg-blue-300 hover:bg-blue-400'block w-full rounded-md py-3 px-4 font-medium text-white shadow  focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-offset-2 focus:ring-offset-gray-900"
+								>
+									Get Started Now
+								</button>
 
 
 							</div>
