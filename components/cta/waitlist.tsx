ed7c177b715b6b00a19593d36ad039dfcf0d1bd6
pnoken <norteypeter@gmail.com>
2024-09-09 13:31:59 +0000

    feat: fiatsend new landing page

--- components/cta/waitlist.tsx
+++ components/cta/waitlist.tsx
@@ -20,7 +20,7 @@ export const Waitlist = () => {
 					</div>
 					<div>
 						<h1 className="py-4 text-5xl font-bold tracking-tight text-center text-transparent bg-gradient-to-t bg-clip-text from-zinc-100/50 to-white sm:text-7xl">
-							Revolutionalizing the future of digital finance.
+							Revolutionizing the future of digital finance.
 						</h1>
 						<p className="mt-6 leading-5 text-yellow-50 sm:text-center">
 							Using fiatsend means decentralization and full compliance. Your data always remains with you. Unencrypted data never leaves your device.
